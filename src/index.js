ca60e4ab7b86939750f40c1cdd4921b3f3e3fc50
qianer-fengtian <perplex0112358@icloud.com>
2020-02-22 11:44:39 +0900

    手番の処理

--- src/index.js
+++ src/index.js
@@ -30,13 +30,19 @@ function Square(props) {
 class Board extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {squares: Array(9).fill(null)}
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    }
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -49,7 +55,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
     return (
       <div>
