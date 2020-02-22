d52b820d8fe728b07f0345b39f8b130a3a9350b1
qianer-fengtian <perplex0112358@icloud.com>
2020-02-22 11:38:33 +0900

    Stateのリフトアップ

--- src/index.js
+++ src/index.js
@@ -14,18 +14,13 @@ import './index.css';
   * 正方形のマス目
   */
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: null};
-  }
-
   render() {
     return (
       <button
         className="square"
-        onClick={() => { this.setState({value: 'X'}) }}
+        onClick={this.props.onClick}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -35,8 +30,24 @@ class Square extends React.Component {
  * 盤面
  */
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {squares: Array(9).fill(null)}
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
